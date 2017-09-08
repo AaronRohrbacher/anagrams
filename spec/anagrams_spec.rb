a591880713024244554f3b245e08cc9535878951
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-08 15:42:22 -0700

    refactor tests to reflect instance variables

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,32 +8,32 @@ describe('title_case') do
   end
 
   it("Accounts for case insensitivity") do
-    anagram = Anagram.new()
-    expect(anagram.check(["Ruby", "Bury"])).to eq(true)
+    anagram = Anagram.new(["Ruby", "Bury"], "Ruby", "Bury")
+    expect(anagram.check()).to eq(true)
   end
 
   it("checks if two words are palindromes") do
-    anagram = Anagram.new()
-    expect(anagram.palindrome_check(["noon", "noon"])).to eq(true)
+    anagram = Anagram.new(["noon", "noon"], "noon", "noon")
+    expect(anagram.palindrome_check()).to eq(true)
   end
 
   it("checks if inputted words contain vowels") do
-    anagram = Anagram.new()
-    expect(anagram.is_word("toe")).to eq(true)
+    anagram = Anagram.new(["tp", "rdl"], "tp", "rdl")
+    expect(anagram.is_word()).to eq(false)
   end
 
   it("checks if words are antigrams") do
-    anagram = Anagram.new()
-    expect(anagram.antigram(["toe", "ply"])).to eq(true)
+    anagram = Anagram.new(["toe", "ply"], "toe", "ply")
+    expect(anagram.antigram()).to eq(true)
   end
 
   it("checks multiple word phrases with punctuation for anagrams") do
-    anagram = Anagram.new()
-    expect(anagram.check(["Aaron. is cool!", "looc is noraa"])).to eq(true)
+    anagram = Anagram.new(["Aaron. is cool!", "looc is noraa"], "Aaron. is cool!", "looc is noraa")
+    expect(anagram.check()).to eq(true)
   end
 
   it("checks multiple word phrases with punctuation for antigrams") do
-    anagram = Anagram.new()
-    expect(anagram.check(["Aaron. is cool!", "looc is noraa"])).to eq(true)
+    anagram = Anagram.new(["Aaron. is cool!", "looc is noraa"], "Aaron. is cool!", "looc is noraa")
+    expect(anagram.check()).to eq(true)
   end
 end
