aecddf4e867dac18d6f47834939175b568328e2e
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-08 09:36:10 -0700

    check if two words are anagrams successfully

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,5 +1,11 @@
 class Anagram
   def check(word_array)
-    return true
+    word1 = word_array[0]
+    word2 = word_array[1]
+    if word1.chars.sort === word2.chars.sort
+      return true
+    else
+      return false
+    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,6 +4,6 @@ require('anagrams')
 describe('title_case') do
   it("checks if two words are anagrams") do
     anagram = Anagram.new()
-    expect(anagram.check("[ruby, bury]")).to eq(true)
+    expect(anagram.check(["ruby", "bury"])).to eq(true)
   end
 end
