e2fff8cd74bfab8cd68f67cba993492f8dffcec7
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-08 10:21:05 -0700

    checks if the two words create a palindrome

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/ruby
+# require('pry')
 class Anagram
   def check(word_array)
     word1 = word_array[0].upcase
     word2 = word_array[1].upcase
     if word1.chars.sort === word2.chars.sort
+      return true
+    else
+      return false
+    end
+  end
+
+  def palindrome_check(word_array)
+    word1 = word_array[0].upcase
+    word2 = word_array[1].upcase
+    phrase = word1 + word2
+    if phrase === phrase.reverse()
       return true
     else
       return false
@@ -19,6 +31,9 @@ word2_input = gets.chomp
 complete_input = [word1_input, word2_input]
 if anagram_check.check(complete_input)
   puts "Hooray! It's an anagram!"
+  if anagram_check.palindrome_check(complete_input)
+    puts "Woah, man- it's also a palindrome."
+  end
 else
   puts "Dah, man! No anagram here."
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -7,8 +7,13 @@ describe('title_case') do
     expect(anagram.check(["ruby", "bury"])).to eq(true)
   end
 
-  it("Account for case insensitivity") do
+  it("Accounts for case insensitivity") do
     anagram = Anagram.new()
     expect(anagram.check(["Ruby", "Bury"])).to eq(true)
+  end
+
+  it("checks if two words are palindromes") do
+    anagram = Anagram.new()
+    expect(anagram.palindrome_check(["noon", "noon"])).to eq(true)
   end
 end
