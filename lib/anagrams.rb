0ed2bd885d466fbcf7f2fa03194072e8d2918e2e
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-08 09:43:31 -0700

    allow scripted user input

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 class Anagram
   def check(word_array)
     word1 = word_array[0]
@@ -8,4 +9,16 @@ class Anagram
       return false
     end
   end
+end
+
+anagram_check = Anagram.new()
+puts "Please enter the first word"
+word1_input = gets.chomp
+puts "Please enter the second word"
+word2_input = gets.chomp
+complete_input = [word1_input, word2_input]
+if anagram_check.check(complete_input)
+  puts "Hooray! It's an anagram!"
+else
+  puts "Dah, man! No anagram here."
 end
